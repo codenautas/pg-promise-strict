0b9e57609dcb80fbf7d6703fece57adccb7fcab3
Emilio <emilioplatzer@gmail.com>
2015-09-04 16:10:26 -0300

    Envío el objeto query a sql-promise

--- pg-promise-strict.js
+++ pg-promise-strict.js
@@ -19,6 +19,10 @@ pgPromiseStrict.debug={};
 
 pgPromiseStrict.defaultPort=5432;
 
+pgPromiseStrict.getQuery = function getQuery(internalConnection, queryArguments){
+    return internalConnection.query.apply(internalConnection,queryArguments);
+}
+
 pgPromiseStrict.Client = function Client(connOpts){
     var client = new pg.Client(connOpts);
     var self=this;
@@ -80,67 +84,6 @@ pgPromiseStrict.queryAdapters = {
             resolve(result);
         }
     })
-};
-
-pgPromiseStrict.Query = function Query(query, client){
-    var self = this;
-    client.motor.allowAccessInternalIfDebugging(self, {query: query, client:client});
-    this.execute = function execute(callbackForEachRow, adapterName){
-        // pgPromiseStrict.log('Query.execute');
-        if(callbackForEachRow && !(callbackForEachRow instanceof Function)){
-            if(adapterName){
-                return Promises.reject(new Error("Query.execute() must recive optional callback function and optional adapterName"));
-            }
-            adapterName=callbackForEachRow;
-            callbackForEachRow=null;
-        }
-        var adapter = pgPromiseStrict.queryAdapters[adapterName||'normal'];
-        return Promises.make(function(resolve, reject){
-            query.on('error',function(err){
-                reject(err);
-            });
-            query.on('row',function(row, result){
-                if(callbackForEachRow){
-                    callbackForEachRow(row, result);
-                }else{
-                    result.addRow(row);
-                }
-            });
-            query.on('end',function(result){
-                result.client = client;
-                if(pgPromiseStrict.log){
-                    pgPromiseStrict.log('-- '+JSON.stringify(result.rows));
-                }
-                adapter(result, resolve, reject);
-            });
-        });
-    };
-    // new functions
-    this.fetchOneRowIfExists = this.execute.bind(this,'upto1');
-    this.fetchUniqueRow      = this.execute.bind(this,'row');
-    this.fetchUniqueValue    = this.execute.bind(this,'value');
-    this.fetchAll            = this.execute.bind(this,'normal');
-    this.fetchRowByRow       = function fetchRowByRow(callback){
-        // pgPromiseStrict.log('Query.onRow');
-        if(!(callback instanceof Function)){
-            var err=new Error('fetchRowByRow must recive a callback that executes for each row');
-            err.code='39004!';
-            return Promises.reject(err);
-        }
-        return this.execute(callback);
-    };
-    this.onRow = this.fetchRowByRow;
-    /* why this then function is needed?
-     *   pg.Client.query is synchronic (not need to recive a callback function) then not need to return a Promise
-     *   but pg-promise-strict always returns a "theneable". Then "then" is here. 
-     */
-    if(pgPromiseStrict.easy){
-        this.then = function then(callback,callbackE){
-            delete this.then;
-            delete this.catch;
-            return this.execute().then(callback,callbackE);
-        };
-    }
 };
 
 pgPromiseStrict.connect = function connect(connectParameters){
