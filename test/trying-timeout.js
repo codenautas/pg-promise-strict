fe99f9ac3e8bd3a916d51ed00e58c5f326f34148
Emilio Platzer <emilioplatzer@gmail.com>
2019-05-05 12:04:10 -0300

    Investigo un poco más el query_timeout

--- test/trying-timeout.js
+++ test/trying-timeout.js
@@ -11,38 +11,36 @@ console.warn(pg.poolBalanceControl());
 
 // DOING skip
 describe.skip('trying timeouts', function(){
+    // waiting for
+    // https://github.com/brianc/node-postgres/issues/1860
     var connectParams = {
         user: 'test_user',
         password: 'test_pass',
         database: 'test_db',
         host: 'localhost',
         port: 5432,
-        releaseTimeout:{ inactive: 800}
+        releaseTimeout:{inactive: 800},
+        query_timeout: 2500
     };
-    it('cancel connect when timeout', function(){
-        return miniTools.readConfig([{db:connectParams}, 'local-config'], {whenNotExist:'ignore'}).then(function(config){
-            return pg.connect(config.db);
-        }).then(function(client){
-            console.log('connected');
-            return client.query("SELECT 1::integer as number_one").execute().then(function(result){
-                console.log('data 1');
-                expect(result.rows).to.eql([{number_one:1}]);
-            }).then(bestGlobals.sleep(1000)).then(function(){
-                //return client.query("SELECT 2::integer as number_one").execute().then(function(result){
-                return client.query("SELECT 2::integer as number_one").then(function(query){
-                    return query.execute();
-                }).then(function(result){
-                    console.log('data 2');
-                    throw new Error("TIMEOUT EXPECTED")
-                },function(err){
-                    console.log('ooooooooook EXEPTION DETECTED')
-                    console.log(err);
-                });
-            });
-        }).catch(function(err){
-            console.log('LAST ERR:',err);
-            throw err;
-        });
+    it('cancel connect when timeout', async function(){
+        var config = await miniTools.readConfig([{db:connectParams}, 'local-config'], {whenNotExist:'ignore'});
+        var client = await pg.connect(config.db);
+        console.log('connected');
+        var result = await client.query("SELECT 1::integer as number_one").fetchAll();
+        console.log('data 1');
+        expect(result.rows).to.eql([{number_one:1}]);
+        try{
+            await bestGlobals.sleep(1000);
+            var result = await client.query("SELECT 2::integer as number_one").fetchAll();
+            console.log('data 2');
+            throw new Error("NO TIMEOUT DETECTED AT THIS POINT")
+        }catch(err){
+            if(err.message!="TIMEOUT EXPECTED"){
+                throw err;
+            }
+            console.log('ooooooooook EXEPTION DETECTED')
+            console.log(err);
+        }
     });
 });
 
